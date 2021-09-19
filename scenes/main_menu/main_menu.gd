987717b8bc0e3c06e363562352dab0f69b21f9a2
Emily <tweetychrip@gmail.com>
2021-09-19 08:02:57 -0700

    Added title screen

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -4,13 +4,9 @@ extends Node
 export(PackedScene) var game_scene
 
 
-func _ready() -> void:
-	$UI/HBox/VBox/Start.grab_focus()
-
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to(game_scene)
-
-
 func _on_meta_clicked(meta) -> void:
 	OS.shell_open(meta)
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to(game_scene)
